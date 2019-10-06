c75dca049f8edaa14dc1567fc3feabffa011d2b7
zwenc <2417935817@qq.com>
2019-10-06 11:35:01 +0800

    修改内容：课程节数改为课程时间 问题描述：无

--- UI/course_add.py
+++ UI/course_add.py
@@ -6,7 +6,7 @@
 from PyQt5.QtWidgets import (QApplication, QWidget, QMessageBox, QMainWindow, QGridLayout, QTableWidgetItem, QCheckBox,
                              QPushButton)
 from UI.ui_course_add import Ui_course_add
-from PyQt5.QtCore import QDate
+from PyQt5.QtCore import QDate, QTime
 from datetime import datetime
 from course import Course
 
@@ -27,41 +27,39 @@ class CourseAdd(QWidget, Ui_course_add):
         else:
             Css_path = "./UI/css/register.css"
 
+        # 加载样式表
         with open(Css_path, "r") as css_file:
             self.setStyleSheet(css_file.read())
 
+        # 初始化周数
         for index in range(20):
             self.comboBox_weeks.addItem(str(index + 1) + "周")
         self.comboBox_weeks.setCurrentIndex(11)
 
         dt = datetime.now()
+        # 初始化日期
         self.dateEdit_startdate.setDate(QDate(dt.year, dt.month, dt.day))
+        self.dateEdit_startdate.setCalendarPopup(True)
 
-        for time_item in [self.comboBox_time1, self.comboBox_time2, self.comboBox_time3]:
-            for index in range(7):
-                if index == 0:
-                    time_item.addItem("")
-                else:
-                    time_item.addItem("第 " + str(2 * index - 1) + "--" + str(2 * index) + " 节课")
-
-        self.comboBox_time1.setCurrentIndex(1)
-        self.comboBox_time2.setCurrentIndex(2)
-        self.comboBox_time3.setCurrentIndex(0)
+        # 初始化时间
+        self.timeEdit_startTime.setTime(QTime(8,30,0))
+        self.timeEdit_startTime.setCalendarPopup(True)
+        self.timeEdit_endTime.setTime(QTime(11,50,0))
 
     def button_register(self, bool):
         if self.lineEdit_course_name.text() == "":
-            QMessageBox(self, "waring", "please enter course's name")
+            QMessageBox.information(self, "waring", "please enter course's name")
             return
 
         if self.lineEdit_course_id.text() == "":
-            QMessageBox(self, "waring", "please enter course's id")
+            QMessageBox.information(self, "waring", "please enter course's id")
             return
 
         print("liuxin 负责编写 数据存储")
 
     def get_course_info(self):
         if self.course == None:
-            QMessageBox(self, "waring", "Don't have any info of course !!")
+            QMessageBox.information(self, "waring", "Don't have any info of course !!")
             return None
 
         return self.course
